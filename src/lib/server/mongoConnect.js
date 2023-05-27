b2293155439c0d12add370a13ccdda0dd1e141f0
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-27 23:27:08 +0200

    fix: :zap: update server address

--- src/lib/server/mongoConnect.js
+++ src/lib/server/mongoConnect.js
@@ -1,9 +1,13 @@
 import { MongoClient } from "mongodb";
+import { envVariables } from "/src/lib/envVariables";
 
 export async function connectToMongoDB() {
 	let client;
 	try {
-		client = new MongoClient("mongodb://127.0.0.1:27017");
+		// client = new MongoClient("mongodb://127.0.0.1:27017");
+		client = new MongoClient(
+			`mongodb+srv://henrikvilhelmberglund:${envVariables.mongoPass}@cluster0.9bryuke.mongodb.net/?retryWrites=true&w=majority`
+		);
 		await client.connect();
 		await client.db("admin").command({ ping: 1 });
 		console.info("Pinged your deployment. You successfully connected to MongoDB!");
