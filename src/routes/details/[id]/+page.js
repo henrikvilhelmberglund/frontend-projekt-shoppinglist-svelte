9b788ef95303d85d07ff7d6583ac2a7bfb557950
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-29 23:20:24 +0200

    feat: :sparkles: add load function for dynamic route

---
+++ src/routes/details/[id]/+page.js
@@ -0,0 +1,18 @@
+/** @type {import('./$types').PageLoad} */
+export async function load({ fetch, params }) {
+	// const auth = await checkLoginStatus(fetch);
+	// if (!auth.user) {
+	// 	return {
+	// 		// instead of throwing an error I return it to the load function so I can display something else than just an error page
+	// 		error: 401,
+	// 	};
+	// }
+
+	const res = await fetch(`/api/${params.id}`);
+	const data = await res.json();
+	// can't serialize BSON ID
+
+	return {
+		list: data,
+	};
+}
