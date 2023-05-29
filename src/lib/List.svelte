752b6482239731817a958fe09a4f34539352789c
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-05-29 23:18:51 +0200

    feat: :sparkles: update List component

--- src/lib/List.svelte
+++ src/lib/List.svelte
@@ -2,13 +2,15 @@
 	export let list;
 
 	console.log(list);
-	const { listname, itemList, color } = list;
+	const { listname, itemList, color, _id } = list;
+
 </script>
 
-<div
+<a
 	class:bg-red-400;!hover-bg-red-500={color === "red"}
 	class:bg-purple-400;!hover-bg-purple-500={color === "purple"}
-	class="m-4 flex h-64 w-64 flex-col items-center rounded bg-slate-300 hover:bg-slate-200">
+	class="m-4 flex h-64 w-64 cursor-pointer flex-col items-center rounded bg-slate-300 shadow shadow-black/50 hover:bg-slate-200"
+  href="/details/{_id}">
 	<h2 class="pt-4 text-2xl font-bold">{listname}</h2>
 	<div class="flex flex-col items-center pt-8">
 		{#each itemList as { itemID: _id, title, checked }}
@@ -17,7 +19,7 @@
 			</p>
 		{/each}
 	</div>
-</div>
+</a>
 
 <style>
 </style>
