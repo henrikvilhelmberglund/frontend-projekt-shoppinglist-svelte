88e56da051bb8be2f1fb65f0dfcc368fc9b1b885
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-03 16:33:52 +0200

    fix: :bug: check if itemList exists before displaying it

--- src/lib/ListDetails.svelte
+++ src/lib/ListDetails.svelte
@@ -6,37 +6,38 @@
 
 	// console.table(list);
 	const { listname, itemList, color, _id } = list;
-
 </script>
 
 <div
 	class:bg-red-400={color === "red"}
 	class:bg-purple-400={color === "purple"}
 	class="flex justify-between shadow shadow-black">
-	<BackButton/>
+	<BackButton />
 	<h1 class="p-6 text-3xl font-bold">{listname}</h1>
 	<div />
 </div>
 
 <div class="p-6 pt-16">
-	{#each itemList as { _id: itemID, title, checked }}
-		<div class="flex flex-row items-center gap-3 pl-2 pt-2">
-			<input
-				on:change={async () => {
-					let newCheckedValue = !checked;
-					console.log(_id, itemID, newCheckedValue);
-					await updateCheckedState(_id, itemID, newCheckedValue);
-				}}
-				class="peer block h-6 w-6 rounded-full text-slate-500 checked:h-6 checked:w-6 checked:bg-slate-500"
-				type="checkbox"
-				{checked}
-				name={title}
-				id="" />
-			<p class="peer-checked:line-through">
-				{title}
-			</p>
-		</div>
-	{/each}
+	{#if itemList}
+		{#each itemList as { _id: itemID, title, checked }}
+			<div class="flex flex-row items-center gap-3 pl-2 pt-2">
+				<input
+					on:change={async () => {
+						let newCheckedValue = !checked;
+						console.log(_id, itemID, newCheckedValue);
+						await updateCheckedState(_id, itemID, newCheckedValue);
+					}}
+					class="peer block h-6 w-6 rounded-full text-slate-500 checked:h-6 checked:w-6 checked:bg-slate-500"
+					type="checkbox"
+					{checked}
+					name={title}
+					id="" />
+				<p class="peer-checked:line-through">
+					{title}
+				</p>
+			</div>
+		{/each}
+	{/if}
 </div>
 
 <style>
