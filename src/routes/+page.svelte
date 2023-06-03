b9408fa9a3a67740d809a751a26f71117cf0b41a
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-03 16:34:21 +0200

    feat: :sparkles: display lists using reactive assignment from data, on deletedList invalidate the data to update the UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,18 +3,32 @@
 	import { base } from "$app/paths";
 	import Footer from "$lib/Footer.svelte";
 	import List from "$lib/List.svelte";
+	import { invalidateAll } from "$app/navigation";
 
 	export let data;
 
-	const listObjects = data.lists;
+	// reactive assignment from loaded data to make sure it is updated when we invalidate
+	$: listObjects = data.lists.reverse();
+
+	let editMode = false;
 </script>
 
 <main class="flex flex-col items-center">
-	<h1 class="p-4 text-3xl font-semibold">My shopping lists</h1>
+	<div class="flex w-full flex-row items-center justify-center">
+		<h1 class="w-fit p-4 text-3xl font-semibold">My shopping lists</h1>
+		<button
+			on:click={() => (editMode = !editMode)}
+			class="i-mdi-dots-vertical absolute right-4 text-3xl" />
+	</div>
 	<hr class="w-full border-black" />
 	<div class="grid grid-cols-2">
-		{#each Object.values(listObjects) as list}
-			<List {list} />
+		{#each Object.values(listObjects) as list (list._id)}
+			<List
+				{list}
+				{editMode}
+				on:deletedList={async () => {
+					await invalidateAll();
+				}} />
 		{/each}
 	</div>
 	<a
