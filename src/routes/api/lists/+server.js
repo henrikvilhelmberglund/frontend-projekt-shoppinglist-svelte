21dee6054ec6d800a98f6aca39392a4b1b52a341
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-03 15:33:41 +0200

    feat: :sparkles: add POST endpoint for /api/lists

--- src/routes/api/lists/+server.js
+++ src/routes/api/lists/+server.js
@@ -1,10 +1,22 @@
 import { json } from "@sveltejs/kit";
 import { getListsCollection } from "../../../hooks.server";
 
+const listsCollection = await getListsCollection();
+
 /** @type {import('./$types').RequestHandler} */
 export async function GET() {
-	const listsCollection = await getListsCollection();
 	const lists = await listsCollection.find({ customfield: "grupp_e" }).toArray();
 
 	return json(lists);
+}
+
+export async function POST({ request }) {
+	const data = await request.json();
+	console.log(data);
+	const lists = await listsCollection.insertOne(data);
+
+	return json({
+		success: true,
+		list: data,
+	});
 }
