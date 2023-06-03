38665341b482320ae0e81492272aec796ce31c30
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-03 16:33:32 +0200

    feat: :sparkles: add deleteList function that deletes a list and dispatches a message deletedList

--- src/lib/List.svelte
+++ src/lib/List.svelte
@@ -1,8 +1,22 @@
 <script>
-	export let list;
+	import { createEventDispatcher } from "svelte";
 
-	console.log(list);
+	export let list;
+	export let editMode;
+
+	// console.log(list);
 	const { listname, itemList, color, _id } = list;
+	const dispatch = createEventDispatcher();
+
+	async function deleteList(id) {
+		const res = await fetch(`/api/lists/${id}`, {
+			method: "DELETE",
+		});
+		const { list } = await res.json();
+		console.log("result:", list);
+		dispatch("deletedList");
+		return list;
+	}
 </script>
 
 <a
@@ -11,6 +25,11 @@
 	class="m-4 flex h-64 w-64 cursor-pointer flex-col items-center rounded bg-slate-300 shadow shadow-black/50 hover:bg-slate-200"
 	href="/details/{_id}">
 	<h2 class="pt-4 text-2xl font-bold">{listname}</h2>
+	<div class="absolute h-64 w-64">
+		<button
+			on:click|preventDefault={async () => await deleteList(_id)}
+			class="i-mdi-delete?bg absolute right--3 top--2 rounded-full bg-slate-400 text-3xl" />
+	</div>
 	<div class="flex flex-col items-center pt-8">
 		{#if itemList}
 			{#each itemList as { itemID: _id, title, checked }}
