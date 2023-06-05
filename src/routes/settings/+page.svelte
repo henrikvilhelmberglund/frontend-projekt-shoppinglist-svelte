e9bd4e0c3d61edd485165fd4da40c9d17411fbc9
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-05 17:23:51 +0200

    feat: :sparkles: add settings route WIP

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import BackButton from "$lib/BackButton.svelte";
+	import Footer from "$lib/Footer.svelte";
+</script>
+
+<main class="flex flex-col items-center">
+	<div class="flex w-full flex-row items-center justify-between">
+		<BackButton />
+		<h1 class="w-fit p-4 text-3xl font-semibold">Settings</h1>
+		<div />
+	</div>
+	<hr class="w-full border-black" />
+</main>
+
+<Footer />
+
+<style>
+</style>
