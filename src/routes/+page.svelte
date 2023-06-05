d50f0b899ab47b80e934ce7e374b15cee8236028
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-05 17:32:31 +0200

    feat: :sparkles: move all routes into [username] dynamic route for each user to have their lists

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,22 @@
 <script>
-	import { afterUpdate } from "svelte";
-	import { base } from "$app/paths";
-	import Footer from "$lib/Footer.svelte";
-	import List from "$lib/List.svelte";
-	import { invalidateAll } from "$app/navigation";
+	import { goto } from "$app/navigation";
 
-	export let data;
-
-	// reactive assignment from loaded data to make sure it is updated when we invalidate
-	$: listObjects = data.lists.reverse();
-
-	let editMode = false;
+	let username;
 </script>
 
-<main class="flex flex-col items-center">
-	<div class="flex w-full flex-row items-center justify-center">
-		<h1 class="w-fit p-4 text-3xl font-semibold">My shopping lists</h1>
-		<button
-			class:shadow-inner;shadow-black={editMode}
-			on:click={() => (editMode = !editMode)}
-			class="i-mdi-dots-vertical?bg absolute right-4 text-3xl" />
-	</div>
-	<hr class="w-full border-black" />
-	<div class="grid grid-cols-2">
-		{#each Object.values(listObjects) as list (list._id)}
-			<List
-				{list}
-				{editMode}
-				on:deletedList={async () => {
-					await invalidateAll();
-				}} />
-		{/each}
-	</div>
-	<a
-		href="/create-list"
-		class="i-mdi-plus?bg mt-4 cursor-pointer rounded-full bg-[hsl(214,32%,89%)] text-5xl shadow-md shadow-black/50 hover:bg-slate-200">
-		<p class="hidden text-transparent">Create list</p>
-	</a>
-</main>
-
-<Footer />
+<div class="flex h-[60vh] items-center justify-center">
+	<input
+		on:keydown={(e) => {
+			if (e.code === "Enter") {
+				goto(`/${username}`);
+			}
+		}}
+		type="text"
+		bind:value={username}
+		required
+		placeholder="Enter your username" />
+	<a class="ml-2 rounded bg-green-400 p-2" href="/{username}">Go to your lists</a>
+</div>
 
 <style>
 </style>
---
+++ src/routes/[username]/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { afterUpdate } from "svelte";
+	import { base } from "$app/paths";
+	import Footer from "$lib/Footer.svelte";
+	import List from "$lib/List.svelte";
+	import { invalidateAll } from "$app/navigation";
+
+	export let data;
+
+	// reactive assignment from loaded data to make sure it is updated when we invalidate
+	$: listObjects = data.lists.reverse();
+
+	let editMode = false;
+</script>
+
+<main class="flex flex-col items-center">
+	<div class="flex w-full flex-row items-center justify-center">
+		<h1 class="w-fit p-4 text-3xl font-semibold">My shopping lists</h1>
+		<button
+			class:shadow-inner;shadow-black={editMode}
+			on:click={() => (editMode = !editMode)}
+			class="i-mdi-dots-vertical?bg absolute right-4 text-3xl" />
+	</div>
+	<hr class="w-full border-black" />
+	<div class="grid grid-cols-2">
+		{#each Object.values(listObjects) as list (list._id)}
+			<List
+				{list}
+				{editMode}
+				on:deletedList={async () => {
+					await invalidateAll();
+				}} />
+		{/each}
+	</div>
+	<a
+		href="/create-list"
+		class="i-mdi-plus?bg mt-4 cursor-pointer rounded-full bg-[hsl(214,32%,89%)] text-5xl shadow-md shadow-black/50 hover:bg-slate-200">
+		<p class="hidden text-transparent">Create list</p>
+	</a>
+</main>
+
+<Footer />
+
+<style>
+</style>
