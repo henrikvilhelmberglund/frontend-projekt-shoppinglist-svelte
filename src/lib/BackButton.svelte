8be9f265450dd30113d06eca5c071d684de8fe42
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-05 17:52:00 +0200

    feat: :sparkles: add username prop to BackButton

--- src/lib/BackButton.svelte
+++ src/lib/BackButton.svelte
@@ -1,8 +1,8 @@
 <script>
-  
+  export let username;
 </script>
 
-<a href="/" class="i-mdi-chevron-left ml-4 text-4xl h-auto">Go back</a>
+<a href="/{username}" class="i-mdi-chevron-left ml-4 text-4xl h-auto">Go back</a>
 
 <style>
   
