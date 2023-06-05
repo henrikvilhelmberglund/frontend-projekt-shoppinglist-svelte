50b74b23b6bd968cf0c387e7752c57ddfecf61e2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-06-05 17:23:19 +0200

    feat: :bento: add favicon, logo

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,11 +1,19 @@
 <script>
-  
 </script>
 
-<header class="bg-slate-300 h-16 w-full">
-  
+<header class="flex h-16 w-full items-center justify-between bg-slate-300">
+	<svg
+		class="ml-2 fill-purple-500"
+		id="logo-85"
+		width="40"
+		height="40"
+		viewBox="0 0 40 40"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"><use xlink:href="logo.svg#logo" /></svg>
+	<a href="/settings" class="i-mdi-gear mr-2 text-3xl">
+		<p class="text-transparent hidden">Settings</p>
+	</a>
 </header>
 
 <style>
-  
 </style>
